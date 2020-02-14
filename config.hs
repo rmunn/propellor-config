9001ae725dc96fbbd1b04868aff4a9a4c9d4abd4
Robin Munn <rmunn@pobox.com>
2020-02-14 17:28:10 +0700

    Add basic Debian Buster (10) box

--- config.hs
+++ config.hs
@@ -1,1 +1,18 @@
-config-simple.hs
+-- This is the main configuration file for Propellor, and is used to build
+-- the propellor program.
+
+import Propellor
+
+main :: IO ()
+main = defaultMain hosts
+
+-- The hosts propellor knows about.
+hosts :: [Host]
+hosts =
+	[ buster
+	]
+
+-- An example host.
+buster :: Host
+buster = host "buster.localnet" $ props
+	& osDebian (Stable "buster") X86_64
